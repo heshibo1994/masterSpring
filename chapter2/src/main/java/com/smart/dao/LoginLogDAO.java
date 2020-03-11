e837078c21e25c4f9b463170da6987233e247364
yif <csuheshibo@163.com>
2020-03-12 01:15:23 +0800

    添加

--- chapter2/src/main/java/com/smart/dao/LoginLogDAO.java
+++ chapter2/src/main/java/com/smart/dao/LoginLogDAO.java
@@ -9,6 +9,7 @@ public class LoginLogDAO {
     //保存登陆日志
     private final static String INSERT_LOGIN_LOG_SQL = "INSERT INTO t_login_log(user_id,ip,login_datetime) VALUES(?,?,?)";
 
+
     public void insertLoginLogSql(LoginLog loginLog){
         Object[] args = {loginLog.getUserId(),loginLog.getIp(),loginLog.getLoginDate()};
         jdbcTemplate.update(INSERT_LOGIN_LOG_SQL,args);
