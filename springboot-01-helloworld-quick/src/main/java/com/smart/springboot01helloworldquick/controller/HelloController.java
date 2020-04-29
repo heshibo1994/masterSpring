17b398679e3abeede0af493d3de6daeab0eda592
yif <csuheshibo@163.com>
2020-04-29 14:44:49 +0800

    springBoot-01-helloworld-quick

---
+++ springboot-01-helloworld-quick/src/main/java/com/smart/springboot01helloworldquick/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.smart.springboot01helloworldquick.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world quick";
+    }
+}
---
+++ springboot-01-helloworld-quick/src/test/java/com/smart/springboot01helloworldquick/Springboot01HelloworldQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.springboot01helloworldquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HelloworldQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
